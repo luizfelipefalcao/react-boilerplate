8b38e3298cd0e345f1102900ad7e8bcb6f86fdf0
Felipe <felipe@gooseinsurance.com>
2024-06-16 22:26:17 -0700

    added server content and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import useFetch from './hooks/useFetch';
 
 function App() {
+  const response = useFetch('http://localhost:8000/users')
+
   return (
     <div className="App">
-      Boilerplate
+      <p>{JSON.stringify(response?.data, null, 4)}</p>
     </div>
   );
 }
